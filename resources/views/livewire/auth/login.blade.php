c85c608d8f1d87281a3d169188f984d64b324756
Saiffil Fariz <1634949+sairiz@users.noreply.github.com>
2025-09-09 19:17:49 +0800

    wip

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -104,7 +104,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
             />
 
             @if (Route::has('password.request'))
-                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
+                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')">
                     {{ __('Forgot your password?') }}
                 </flux:link>
             @endif
@@ -121,7 +121,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
     @if (Route::has('register'))
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
             <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+            <flux:link :href="route('register')">{{ __('Sign up') }}</flux:link>
         </div>
     @endif
 </div>
